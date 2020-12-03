b6b7cb26ca2c303cff71e418b1126530bdb0f4d2
mac-mini <lexx.mg@gmail.com>
2020-12-03 21:47:03 +0300

    add module css header

--- my-app/src/components/header/Header.jsx
+++ my-app/src/components/header/Header.jsx
@@ -1,7 +1,9 @@
+import s from './style.module.css';
+
 const Header = () => {
   return (
-    <header className="header">
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYEUvgYC6_i_LzHV9-qVIJRLhkGA7jnprtUw&usqp=CAU" alt="logo" className="header__img"/>
+    <header className={s.header}>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYEUvgYC6_i_LzHV9-qVIJRLhkGA7jnprtUw&usqp=CAU" alt="logo" className={s.img}/>
     </header>
   )
 }
