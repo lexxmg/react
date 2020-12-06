d7f3bd09099c9cb16a845379856aea5833ccdbd2
mac-mini <lexx.mg@gmail.com>
2020-12-06 14:32:13 +0300

    sync

--- my-app/src/components/dialogs/Dialogs.jsx
+++ my-app/src/components/dialogs/Dialogs.jsx
@@ -1,6 +1,38 @@
 import { NavLink } from 'react-router-dom';
 import './dialogs.css';
-import Post from '../main/post/Post';
+
+const UserLink = (props) => {
+  let path = '';
+  const id = props.id;
+
+  if ( props.path ) {
+    path = props.path;
+  } else {
+    path = '/message';
+  }
+
+  return (
+    <li className="dialogs-list__item">
+      <NavLink to={path + '/' + id} className="dialogs-list__link">{props.userName}</NavLink>
+    </li>
+  );
+}
+
+const Dialog = props => {
+  return (
+    <div className="dialogs__mess dialogs-mess">
+      <div className="dialogs-mess__avatar">
+        <img src="" alt="аватар" className="dialogs-mess__img"/>
+
+        <span className="dialogs-mess__user-name">{props.name}</span>
+      </div>
+
+      <div className="dialogs-mess__posts">
+        <p className="dialogs-mess__text">{props.text}</p>
+      </div>
+    </div>
+  )
+}
 
 const Dialogs = (props) => {
   return (
@@ -10,41 +42,24 @@ const Dialogs = (props) => {
       <div className="dialogs__container">
         <div className="dialogs__user">
           <ul className="dialogs__list dialogs-list">
-            <li className="dialogs-list__item">
-              <NavLink to="/message/1" className="dialogs-list__link">userName-1</NavLink>
-            </li>
-            <li className="dialogs-list__item">
-              <NavLink to="/message/2" className="dialogs-list__link">userName-1</NavLink>
-            </li>
-            <li className="dialogs-list__item">
-              <NavLink to="/message/3" className="dialogs-list__link">userName-1</NavLink>
-            </li>
-            <li className="dialogs-list__item">
-              <NavLink to="/message/4" className="dialogs-list__link">userName-1</NavLink>
-            </li>
-            <li className="dialogs-list__item">
-              <NavLink to="/message/5" className="dialogs-list__link">userName-1</NavLink>
-            </li>
-            <li className="dialogs-list__item">
-              <NavLink to="/message/6" className="dialogs-list__link">userName-1</NavLink>
-            </li>
+            <UserLink id="1" userName="Vasya dfg"/>
+            <UserLink id="2" userName="Vasya 425"/>
+            <UserLink id="3" userName="Vasya fd"/>
+            <UserLink id="4" userName="Vasya fd"/>
+            <UserLink id="5" userName="Vasya dfsh"/>
+            <UserLink id="6" userName="Vasya dh"/>
+            <UserLink id="7" userName="Vasyasdf"/>
+            <UserLink id="8" userName="Vasya dfhdj"/>
           </ul>
         </div>
 
         <div className="dialogs__message">
-          <div className="dialogs__mess dialogs-mess">
-            <div className="dialogs-mess__avatar">
-              <img src="" alt="аватар" className="dialogs-mess__img"/>
-
-              <span className="dialogs-mess__user-name">UserName</span>
-            </div>
-
-            <div className="dialogs-mess__posts">
-              <p className="dialogs-mess__text">Lorem+5jhhhhkh jhk jh Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem voluptatibus facilis nulla voluptatum, deserunt tenetur eveniet saepe, id provident ducimus, dolorum nesciunt quia, ad commodi iste ullam adipisci temporibus perspiciatis.</p>
-            </div>
-          </div>
-
-          <Post text="rfrjqnj ag" />
+          <Dialog name="Vasya" text="какой то текст какой то текст какой то текст какой то текст какой то текст"/>
+          <Dialog name="Vasya" text="какой то текст какой то текст какой то текст текст какой то текст"/>
+          <Dialog name="Vasya" text="какой то текст какой то текст какой то текст какой то"/>
+          <Dialog name="Vasya" text="какой то текст какой то текст какой то текст какой то текст какой то текст"/>
+          <Dialog name="Vasya" text="какой то текст какой то текст какой то текст текст какой то текст"/>
+          <Dialog name="Vasya" text="какой то текст какой то текст какой то текст какой то"/>
         </div>
       </div>
     </div>
