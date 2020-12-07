daf3f013b0f485096cd583e891b2d7b59be8fae9
mac-mini <lexx.mg@gmail.com>
2020-12-07 15:40:03 +0300

    sync

--- my-app/src/components/dialogs/Dialogs.jsx
+++ my-app/src/components/dialogs/Dialogs.jsx
@@ -2,8 +2,22 @@
 import './dialogs.css';
 import UserList from './userList/UserList';
 import UserDialog from './userDialog/UserDialog';
+import SendMessage from './sendMessage/SendMessage';
 
 const Dialogs = (props) => {
+  const dialogArr = props.dialog.map((obj) => {
+    return (
+      <UserDialog key={obj.id} name={obj.name} text={obj.post} />
+    )
+  });
+
+  const userArr = props.users.map((obj) => {
+    return (
+      <UserList key={obj.id} id={obj.id} userName={obj.name} avatar={obj.avatar} />
+    )
+  });
+
+
   return (
     <div className="doalogs">
       <h2 className="dialogs__title">Диалоги</h2>
@@ -11,24 +25,14 @@ const Dialogs = (props) => {
       <div className="dialogs__container">
         <div className="dialogs__user">
           <ul className="dialogs__list dialogs-list">
-          {
-            props.users.map((obj) => {
-              return (
-                <UserList key={obj.id} id={obj.id} userName={obj.name} avatar={obj.avatar} />
-              )
-            })
-          }
+            { userArr }
           </ul>
         </div>
 
         <div className="dialogs__message">
-        {
-          props.dialog.map((obj) => {
-            return (
-              <UserDialog key={obj.id} name={obj.name} text={obj.post} />
-            )
-          })
-        }
+          { dialogArr }
+
+          <SendMessage />
         </div>
       </div>
     </div>
---
+++ my-app/src/components/dialogs/sendMessage/SendMessage.jsx
@@ -0,0 +1,28 @@
+
+import React from 'react';
+import './send.css';
+
+const newPostElement = React.createRef();
+
+const SendMessage = (props) => {
+  return (
+    <div className="dialogs__send dialogs-send">
+      <form className="dialogs-send__form" onSubmit={ event => sendMessage(event) }>
+        <textarea className="dialogs-send__text" ref={ newPostElement }></textarea>
+
+        <button className="dialogs-send__btn"></button>
+      </form>
+    </div>
+  );
+}
+
+function sendMessage(e) {
+  e.preventDefault();
+
+  const text = newPostElement.current.value;
+
+  alert(text);
+  newPostElement.current.value = '';
+}
+
+export default SendMessage;
