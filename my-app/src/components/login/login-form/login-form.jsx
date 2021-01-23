d64106444941ecc7a55e2ec649fe8c845a66813b
mac-mini <lexx.mg@gmail.com>
2021-01-23 12:18:48 +0300

    add form

---
+++ my-app/src/components/login/login-form/login-form.jsx
@@ -0,0 +1,14 @@
+
+import './login-form.css';
+
+const LoginForm = (props) => {
+  return (
+    <form className="login-form">
+      <div className="login-form__input-wrapper">
+        <input type="text" className="login-form__input"/>
+      </div>
+    </form>
+  )
+}
+
+export default LoginForm;
