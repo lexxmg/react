7dbcb6f1469eb1003c10b6cdd8b58aecb01d7476
mac-book <lexx.mg@gmail.com>
2021-02-04 17:56:33 +0300

    fix

--- my-app/src/components/common/FormsControls/Input/Input.jsx
+++ my-app/src/components/common/FormsControls/Input/Input.jsx
@@ -7,8 +7,8 @@ export const Input = ({meta, input, ...props}) => {
     <div className="form-input">
       <input {...input} {...props}
         className={ (meta.error && meta.touched)
-                    ? "form-input__input form-input__input--err"
-                    : "form-input__input"
+                    ? props.className + " " + "form-input__input form-input__input--err"
+                    : props.className + " " + "form-input__input"
                   }
       />
       {
