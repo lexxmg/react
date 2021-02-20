e9be6aaff4461d1565e9adf2e55ac9052a61ce17
mac-book <lexx.mg@gmail.com>
2021-02-20 14:49:56 +0300

    first commit

--- my-app/src/components/main/profileStatus/ProfileStatusHook.jsx
+++ my-app/src/components/main/profileStatus/ProfileStatusHook.jsx
@@ -1,95 +1,25 @@
 
 import './profileStatus.css';
-import React, { useState } from 'react';
-
-// class ProfileStatusHook extends React.Component {
-//   constructor(props) {
-//     super(props);
-//
-//     this.state = {
-//       select: true,
-//       editMode: false,
-//       inputValue: this.props.userStatus
-//     }
-//   }
-//
-//   componentDidUpdate(prevProps, prevStste) {
-//     // console.log(prevProps.userStatus + 'prevProps');
-//     // console.log(this.props.userStatus + 'props');
-//     const input = document.querySelector('.profileStatus__input');
-//
-//     if (input) {
-//       //console.log(input);
-//       if (this.state.select) {
-//         input.focus();
-//         input.select();
-//         this.setState({
-//           select: false
-//         })
-//       }
-//     }
-//     if (prevProps.userStatus !== this.props.userStatus) {
-//       this.setState({
-//         inputValue: this.props.userStatus
-//       });
-//     }
-//   }
-//
-//   activateEditMode = () => {
-//     if ( this.props.currentProfile.userId === this.props.authId ) {
-//       this.setState({
-//         editMode: true
-//       });
-//     }
-//   }
-//
-//   deactivateEditMode = (event) => {
-//     const status = event.target.value;
-//
-//     this.setState({
-//       editMode: false,
-//       select: true
-//     });
-//
-//     this.props.updateUserStatus(status);
-//   }
-//
-//   setValue(event) {
-//     event.preventDefault();
-//     const value = event.target.value;
-//
-//     this.setState({
-//       inputValue: value
-//     });
-//   }
-//
-//   render() {
-//     return (
-//       <div className="profileStatus">
-//         {
-//           this.state.editMode
-//           ? <input type="text"
-//               className="profileStatus__input"
-//               value={this.state.inputValue}
-//               onChange={(event) => this.setValue(event)}
-//               onBlur={this.deactivateEditMode}
-//           />
-//           : <span className="profileStatus__status"
-//               onClick={this.activateEditMode}
-//             >{this.props.userStatus || 'Нет статуса'}
-//           </span>
-//         }
-//       </div>
-//     )
-//   }
-// }
-
+import React, { useState, useEffect } from 'react';
 
 
 const ProfileStatusHook = (props) => {
 
   const [editMode, setEditMode] = useState(false);
   const [inputValue, setInputValue] = useState(props.userStatus);
+
+  useEffect(() => {
+    const input = document.querySelector('.profileStatus__input');
+
+    if (input) {
+      input.focus();
+      input.select();
+    }
+  }, [editMode]);
+
+  useEffect(() => {
+    setInputValue(props.userStatus);
+  }, [props.userStatus]);
 
   const setValue = (event) => {
     const value = event.target.value;
