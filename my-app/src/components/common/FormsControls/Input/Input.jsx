03500063468f3e0db5ca78babc1a5103c55f58da
mac-book <lexx.mg@gmail.com>
2021-03-08 17:28:30 +0300

    edit input add classnames

--- my-app/src/components/common/FormsControls/Input/Input.jsx
+++ my-app/src/components/common/FormsControls/Input/Input.jsx
@@ -1,5 +1,26 @@
 
 import './inputForm.css';
+import cn from 'classnames';
+
+// export const Input = ({meta, input, ...props}) => {
+//   //console.log(props);
+//   //console.log(meta);
+//   return (
+//     <div className="form-input">
+//       <input {...input} {...props}
+//         className={ (meta.touched && meta.error)
+//                     ? props.className + ` form-input__input form-input__input--err`
+//                     : props.className + ` form-input__input`
+//                   }
+//       />
+//       {
+//         (meta.touched && meta.error)
+//         ? <span className="form-input__text">{meta.error}</span>
+//         : ''
+//       }
+//     </div>
+//   )
+// }
 
 export const Input = ({meta, input, ...props}) => {
   //console.log(props);
@@ -7,10 +28,7 @@ export const Input = ({meta, input, ...props}) => {
   return (
     <div className="form-input">
       <input {...input} {...props}
-        className={ (meta.touched && meta.error)
-                    ? props.className + ` form-input__input form-input__input--err`
-                    : props.className + ` form-input__input`
-                  }
+        className={ cn(props.className, 'form-input__input', {'form-input__input--err': meta.touched && meta.error} ) }
       />
       {
         (meta.touched && meta.error)
